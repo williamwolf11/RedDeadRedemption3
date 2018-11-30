b50a9f733f8f22446d1ae5d4aaa1b7080a24388a
Sophie Smith <slsmith@middlebury.edu>
2018-11-30 12:01:14 -0500

    Cowboys (that are actually circles)

---
+++ src/Cowboys.java
@@ -0,0 +1,23 @@
+
+import java.awt.*;
+
+public class Cowboys {
+	
+	int x;     // position
+    int y;
+    int size = 5;
+    
+    public Cowboys(int screenX, int screenY){ // creates ball with random attributes
+        x = (int)(Math.random()*screenX)+0;
+        y = (int)(Math.random()*screenY)+0;
+        // size = (int)(Math.random()*8)+8;
+    }
+    
+    public void draw(Graphics g) {
+        g.setColor(Color.blue);
+        g.fillOval(x - size, y - size, 2*size, 2*size);
+        g.setColor(Color.black);
+        g.drawOval(x - size, y - size, 2*size, 2*size);
+    }
+    
+}
