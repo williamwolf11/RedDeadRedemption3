7140fe3dad745e318027e9cdce589204bc2b1c0b
williamwolf11 <wwolf@middlebury.edu>
2018-12-07 17:12:20 -0500

    Last push, here are the images

--- src/Cowboys.java
+++ src/Cowboys.java
@@ -1,3 +1,8 @@
+// Red Dead Redemption 3
+// Cowboys.java
+// Names: Sophie Smith and William Wolf
+// CSCI0201 Final Project
+// Dec 7, 2018
 
 import java.awt.*;
 
@@ -6,6 +11,7 @@ import java.awt.*;
 public class Cowboys {
     int x;     // position
     int y;
+    Image cowboyChoice;
     
     // creates cowboy with random position 
     // x coordinates between 50 and 950
@@ -13,13 +19,37 @@ public class Cowboys {
     public Cowboys(){ 
         x = (int)(Math.random()*900) + 50;
         y = (int)(Math.random()*250) + 250;
+        cowboyChoice = imageChoice();
         }
     
     // draws cowboy at x and y position. Size of cowboy depends on 
     // y coordinate to create depth in game board
-    public void drawCowboy(Graphics g, Image img) {
+    public void drawCowboy(Graphics g) {
+    	
     	long size =  (long)y / (long)5;
     	int roundedSize = (int)size;
-        g.drawImage(img, x, y, roundedSize, roundedSize*2, null);
+        g.drawImage(cowboyChoice, x, y, roundedSize, roundedSize*2, null);
+    }
+    
+    // Randomly chooses an image to be added to the vector
+    public Image imageChoice() {
+
+    	double numChoice = (Math.random()*10);
+    	if (numChoice <= 2) {
+    		return RedTest.dutchImg;
+    	}
+    	if (numChoice <= 4) {
+    		return RedTest.allendeImg;
+    	}
+    	if (numChoice <= 6) {
+    		return RedTest.javierImg;
+    	}
+    	if (numChoice <= 8) {
+    		return RedTest.nigelImg;
+    	}
+    	else{
+    		return RedTest.haroldImg;
+    	}
+    	 
     }
 }
--- src/RedTest.java
+++ src/RedTest.java
@@ -16,17 +16,21 @@ public class RedTest extends Applet implements ActionListener{
 	
 	// images for the game
 	static Image mainCharImg;
-	static Image cowboyImg;
+	static Image dutchImg;
+	static Image javierImg;
+	static Image haroldImg;
+	static Image allendeImg;
+	static Image nigelImg;
 	static Image backgroundImg;
-	
+
     protected RedDeadCanvas c;
     protected Button restartButton;
     protected Label titleLabel, scoreLabel;
     protected int currentScore;
     
-    
     static final Color dgreen = new Color(0, 120, 90);
     
+    // initializes game set up and assigns image instance variables
     public void init () {
         setLayout(new BorderLayout());
         add("North", makeTopControlPanel());
@@ -36,28 +40,37 @@ public class RedTest extends Applet implements ActionListener{
         add("Center", c);
         
         backgroundImg = getImage(getDocumentBase(), "images/background1.jpg");
+        // 
         mainCharImg = getImage(getDocumentBase(), "images/Marston.png");
         //Found at http://convergence-series.wikia.com/wiki/John_Marston
-        cowboyImg = getImage(getDocumentBase(), "images/CSImage3.png");
+        dutchImg = getImage(getDocumentBase(), "images/CSImage3.png");
+        allendeImg = getImage(getDocumentBase(), "images/Coronel_allende.png");
+        haroldImg = getImage(getDocumentBase(), "images/Professor.png");
+        javierImg = getImage(getDocumentBase(), "images/javierescuella.png");
+        nigelImg = getImage(getDocumentBase(), "images/NigelWestDickens.png");
+        // Previous 4 images found at http://reddead.wikia.com
         
         currentScore = 0;    
     }
-    
+    // starts canvas
     public void start() {
         c.start();
     }
 
+    // stops canvas
     public void stop() {
         c.stop();
     }
     
-    
+    // handles response to button
     public void actionPerformed(ActionEvent e) {
+    	// restarts game if restartButton is pressed
         if (e.getSource() == restartButton) {
            restart();
         }
     }
-    
+    // makes the top panel of the game with score label,
+    // title label, and restart button
     public Panel makeTopControlPanel() {
     	Panel topControlPanel = new Panel();
     	topControlPanel.setLayout(new GridLayout(1, 3));
@@ -75,6 +88,8 @@ public class RedTest extends Applet implements ActionListener{
         return topControlPanel;
     }
     
+    // creates title label with the name of game in bold red
+    // lettering
     public Label makeTitleLabel() {
     	titleLabel = new Label("Red Dead Redemption 3");
     	titleLabel.setFont(new Font("Helvetica", Font.BOLD, 16));
@@ -83,6 +98,7 @@ public class RedTest extends Applet implements ActionListener{
     	return titleLabel;
     }
     
+    // makes the score label in white plain font
     public Label makeScoreLabel() {
     	scoreLabel = new Label("Score: " + Integer.toString(currentScore));
     	scoreLabel.setFont(new Font("Helvetica", Font.PLAIN, 16));
@@ -91,6 +107,7 @@ public class RedTest extends Applet implements ActionListener{
         return scoreLabel;
     }
     
+    // creates the restart button in dark green font
     public Button makeRestartButton() {
     	restartButton = new Button("Restart");
     	restartButton.setFont(new Font("Helvetica", Font.PLAIN, 16));
@@ -117,28 +134,31 @@ class RedDeadCanvas extends Canvas implements Runnable, MouseListener,  MouseMot
 		
 	RedTest parent; //instance variable to be able to access applet components
 	static Vector<Cowboys> cowboys;
+	private Thread t = null;
     int x; // position of target
     int y;
+    int speed; // speed that cowboys appear
+    
     boolean blackOrColor = true; // color of circle (true = black, false = colored)
     boolean goodHit = false; // whether the click was a hit 
-    int speed;
-    private Thread t = null;
-    long starttime;
+    
     int mainCharPos = 432;
+    
+    // size of target
     int targetRadius = 20;
     int centerDotRad = 4;
-    //constant int cowboysPosition = 500;
+    int targStartLine = 25;
+    int targEndLine = 15;
 
-    
     public RedDeadCanvas(RedTest s) {
     	parent = s;
     	newGame();
     }
     
+    // resets components of the game
     public void newGame() {
     	speed = 3000;
     	cowboys = new Vector<Cowboys>();
-    	cowboys.add(new Cowboys());
     }
 
 	//draws the canvas
@@ -154,7 +174,7 @@ class RedDeadCanvas extends Canvas implements Runnable, MouseListener,  MouseMot
         		g.setColor(Color.lightGray);
         	} 
         }
-        
+        // background image
         g.drawImage(RedTest.backgroundImg, 0, 0, null);
         //puts image of John Marston in the corner
         g.drawImage(RedTest.mainCharImg, 0, mainCharPos, null);
@@ -163,29 +183,29 @@ class RedDeadCanvas extends Canvas implements Runnable, MouseListener,  MouseMot
         //Found this at https://stackoverflow.com/questions/16995308/can-you-increase-
         //line-thickness-when-using-java-graphics-for-an-applet-i-dont?lq=1
        
-        //draws target around mouse location
-        //run(g);
+        // draws cowboys
         for (int i=0; i<cowboys.size(); i++) {
     		Cowboys c = cowboys.get(i);
-    		c.drawCowboy(getGraphics(), RedTest.cowboyImg);
+    		c.drawCowboy(getGraphics());
     	}
+        // draws the target following cursor
 		g2.setStroke(new BasicStroke(3));
 		g2.drawOval(x-targetRadius, y-targetRadius, 2*targetRadius, 2*targetRadius);
-		g2.drawLine(x-25, y, x-15, y);
-		g2.drawLine(x+25, y, x+15, y);
-		g2.drawLine(x, y-25, x, y-15);
-		g2.drawLine(x, y+25, x, y+15);
+		g2.drawLine(x-targStartLine, y, x-targEndLine, y);
+		g2.drawLine(x+targStartLine, y, x+targEndLine, y);
+		g2.drawLine(x, y-targStartLine, x, y-targEndLine);
+		g2.drawLine(x, y+targStartLine, x, y+targEndLine);
 		g.fillOval(x-centerDotRad, y-centerDotRad, 2*centerDotRad, 2*centerDotRad);
     }
-    
-    
+   
+    // starts the thread
     public void start() {
     	if (t == null) {
     		t = new Thread(this);
     		t.start();
     	}
     }
-
+    // stops the thread
 	@SuppressWarnings("deprecation")
 	public void stop() {
     	t.stop();
@@ -194,28 +214,35 @@ class RedDeadCanvas extends Canvas implements Runnable, MouseListener,  MouseMot
 
     public void run(){
     	Thread currentThread = Thread.currentThread();
+    	// while running
         while (currentThread == t) {
+        	// game stops if there are 15 cowboys
         	while(cowboys.size() < 15) {
-        		if (System.currentTimeMillis()%speed <= 1) {
+        		
+        		if (System.currentTimeMillis()%speed == 0) {
         			cowboys.add(new Cowboys());
-        			if(speed > 500) {
+        			repaint();
+        			// makes cowboys appear faster as game goes on
+        			if(speed > 100) {
         				speed-=20;
+        			
         			}
         		}
-        		repaint();
+        		
         	}
+        	// when 15 cowboys appear, shows Game Over message
     		parent.scoreLabel.setText("Game Over! Score: " + parent.currentScore);
         }
         
     }
     
-    // toggles color of circle and repaints
+    // toggles color of target and repaints
     public void toggleColor() {
         blackOrColor = ! blackOrColor;
         repaint();
     }
 
-    // draws color circle where mouse is pressed
+    // shoots and determines if shot was a hit
     public void mousePressed(MouseEvent event) {
         Point p = event.getPoint();
         x = p.x;
@@ -223,21 +250,27 @@ class RedDeadCanvas extends Canvas implements Runnable, MouseListener,  MouseMot
 
         for (int i=0; i<cowboys.size(); i++) {
 			Cowboys c = cowboys.get(i);
+			// if the position where player clicks is on head/shoulders of cowboy
 			if(p.x <= c.x+57 && p.x >= c.x+20 && p.y <= c.y+95 && p.y >= c.y+10 && cowboys.size() < 15) {
 				cowboys.remove(i--);
+				// changes color of target to red
 				goodHit = true;
+				// add one to score and update label
 				parent.currentScore += 1;
 				parent.scoreLabel.setText("Score: " + parent.currentScore);
 			}
 			repaint();
 		}
+        // when mouse is pressed, target is either gray or red
         toggleColor();
         repaint(); // redraws canvas (yellow) and draws circle
     }
     
     // when mouse is released, target turns back to black
     public void mouseReleased(MouseEvent event) {
+    	// resets goodHit boolean 
     	goodHit = false;
+    	// target turns black again
     	toggleColor();
     }
     
@@ -246,22 +279,16 @@ class RedDeadCanvas extends Canvas implements Runnable, MouseListener,  MouseMot
     public void mouseEntered(MouseEvent event) { }
     
     public void mouseExited(MouseEvent event) { }
-
+    
     public void mouseDragged(MouseEvent event)  { }
     
-
+    // target follows cursor around
     public void mouseMoved(MouseEvent event) {
-    	// target follows cursor around
     	Point p = event.getPoint();
         x = p.x;
         y = p.y;
         repaint();
     }
-	
-	public static void clearVector() {
-		cowboys.clear();
-	}
-	
 	
 }
 	
